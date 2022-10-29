b010a7312a4f531b8811bcc8db117a2629e3a40f
Mauricio Yemi Martins <72272047+mauricioyemi@users.noreply.github.com>
2022-10-29 17:09:17 -0300

    Update CelsiusFahrenheit.py

--- CelsiusFahrenheit.py
+++ CelsiusFahrenheit.py
@@ -1,9 +1,7 @@
 # Python Program to convert temperature in celsius to fahrenheit
 
-celsius = input("Enter a temperature in Celsius: ")
-cel = int(celsius)
-
-fahrenheit = (cel * 1.8) + 32
+celsius = int(input("Enter a temperature in Celsius: "))
+fahrenheit = (celsius * 1.8) + 32
 print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(cel,fahrenheit))
 
 
